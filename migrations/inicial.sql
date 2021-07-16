a86d44674d1d58a03e263949be26b13625feeee1
André Marcondes Teixeira <andremarcondesteixeira@users.noreply.github.com>
2021-07-15 21:42:02 -0300

    Update and rename 202104261220-inicial.sql to inicial.sql

--- migrations/inicial.sql
+++ migrations/inicial.sql
@@ -1,6 +1,6 @@
 -- drop database gestao_financeira;
-create database gestao_financeira;
-use gestao_financeira;
+create database greenwallet;
+use greenwallet;
 
 create table users (
     id int AUTO_INCREMENT primary key,
@@ -9,8 +9,8 @@ create table users (
     password varchar(100) not null,                 -- Senha do usuario
     activation_code varchar(4),                     -- Codigo de ativação da conta / Recuperação de Senha
     state char default 'P',                         -- [A] Ativo / [P] Pendente / [D] Desativado (a) / [E] Excluido (a)
-    update_at datetime on update current_timestamp,
-    created_at datetime default current_timestamp
+    created_at datetime default current_timestamp,
+    update_at datetime on update current_timestamp
 ) 
 engine = INNODB 
 default charset = UTF8MB4;
@@ -21,8 +21,8 @@ create table categories (
     name varchar(100) not null,                     -- Nome da Categoria ( Obrigatorio )
     enabled boolean default 1 not null,             -- 1 = True = Enabled / 0 = False = Disabled
     type char not null,                             -- [E] Despesas / [R] Receitas
-    update_at datetime on update current_timestamp,
     created_at datetime default current_timestamp,
+    update_at datetime on update current_timestamp,
     FOREIGN KEY (user_id) REFERENCES users(id)
 ) 
 engine = INNODB 
@@ -38,8 +38,8 @@ create table transactions (
     due_date date not null,                         -- Data de Vencimento ou Data de Recebimento
     type char not null,                             -- Tipo da transação: [E] Expense (Despesas) / [R] Revenue (Receitas)
     state char default 'A',                         -- [A] Ativo / [D] Desativado (a) / [E] Excluido (a)
-    update_at datetime on update current_timestamp,
     created_at datetime default current_timestamp,
+    updated_at datetime on update current_timestamp,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 )
